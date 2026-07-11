1b97b341d8843d81951b3edf899aa2714c9d061c
Minhtamdev31 <tamtran11037469@gmail.com>
2026-07-11 18:01:23 +0700

    fix to run on phone

--- pizza_flutter/lib/core/constants.dart
+++ pizza_flutter/lib/core/constants.dart
@@ -1,5 +1,6 @@
 class AppConstants {
-  // Web/Windows: dùng localhost. Android Emulator: đổi thành http://10.0.2.2:5211/api
-  // Thiết bị thật: đổi thành IP máy tính, VD: http://192.168.1.5:5211/api
-  static const String baseUrl = 'http://localhost:5211/api';
+  // Android Emulator: 10.0.2.2 = localhost của máy tính.
+  // Web/Windows: đổi thành http://localhost:5211/api
+  // Thiết bị thật: đổi thành IP LAN máy tính, VD: http://192.168.1.5:5211/api
+  static const String baseUrl = 'http://10.0.2.2:5211/api';
 }
